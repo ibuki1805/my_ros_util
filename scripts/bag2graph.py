346d91d1d7331c6703abd07cd32f5997479fafd9
ibuki1805 <ee173131@meiji.ac.jp>
2023-02-01 08:26:06 +0900

    update time-velocity graph

--- scripts/bag2graph.py
+++ scripts/bag2graph.py
@@ -16,9 +16,10 @@ class BagReader:
     def __init__(self, bagfile_path: str, file_name: str) -> None:
         self.bagfile_path = bagfile_path
         self.conditions = self.get_conditions_from_file_name(file_name)
-        self.pose_data = {"time": [], "x": [], "y": [], "z": [], "roll": [], "pitch": [], "yaw": [], "roll_deg": [], "pitch_deg": [], "yaw_deg": []}
+        self.pose_data = {"time": [], "x": [], "y": [], "z": [], "roll": [], "pitch": [], "yaw": [], "roll_deg": [], "pitch_deg": [], "yaw_deg": [], "dt": [], "v": [], "vx": [], "vy": [], "yaw_rate": [], "yaw_rate_deg": []}
         self.target_trajectory = {"x": [], "y": []}
         self.steer_data = {"time": [], "left": [], "right": [], "left_deg": [], "right_deg": []}
+        self.peson0_data = {"time": [], "x": [], "y": [], "z": [], "roll": [], "pitch": [], "yaw": [], "roll_deg": [], "pitch_deg": [], "yaw_deg": []}
 
     def dxl2steer_angle(self) -> dict:
         with rosbag.Bag(self.bagfile_path) as bag:
@@ -48,15 +49,34 @@ class BagReader:
 
     def tf2pose(self) -> dict:
         with rosbag.Bag(self.bagfile_path) as bag:
+            is_first_loop = True
             for topic, msg, t in bag.read_messages(topics=["/tf"]):
                 for transform in msg.transforms:
                     if transform.child_frame_id == "vicon/ccv/ccv":
-                        _time = transform.header.stamp.to_sec()
+                        if is_first_loop:
+                            offsetTime = transform.header.stamp.to_sec()
+                            is_first_loop = False
+                        _time = transform.header.stamp.to_sec() - offsetTime
                         _x = transform.transform.translation.x
                         _y = transform.transform.translation.y
                         _z = transform.transform.translation.z
                         _rotation = transform.transform.rotation
                         _roll, _pitch, _yaw = euler_from_quaternion([_rotation.x, _rotation.y, _rotation.z, _rotation.w])
+                        if len(self.pose_data["time"]) == 0 :
+                            _dt = 0.0
+                            _vx = 0.0
+                            _vy = 0.0
+                            _v = 0.0
+                            _yaw_rate = 0.0
+                            _yaw_rate_deg = 0.0
+                        else :
+                            _dt = _time - self.pose_data["time"][-1]
+                            _vx = (_x - self.pose_data["x"][-1]) / _dt
+                            _vy = (_y - self.pose_data["y"][-1]) / _dt
+                            _v = math.sqrt(_vx**2 + _vy**2)
+                            _yaw_rate = (_yaw - self.pose_data["yaw"][-1]) / _dt
+                            _yaw_rate_deg = _yaw_rate * 180 / math.pi
+
                         _roll_deg = _roll * 180 / math.pi
                         _pitch_deg = _pitch * 180 / math.pi
                         _yaw_deg = _yaw * 180 / math.pi
@@ -70,13 +90,14 @@ class BagReader:
                         self.pose_data["roll_deg"].append(_roll_deg)
                         self.pose_data["pitch_deg"].append(_pitch_deg)
                         self.pose_data["yaw_deg"].append(_yaw_deg)
+                        self.pose_data["dt"].append(_dt)
+                        self.pose_data["vx"].append(_vx)
+                        self.pose_data["vy"].append(_vy)
+                        self.pose_data["v"].append(_v)
+                        self.pose_data["yaw_rate"].append(_yaw_rate)
+                        self.pose_data["yaw_rate_deg"].append(_yaw_rate_deg)
 
-        for i in range(len(self.pose_data["x"])):
-            if i == 0:
-                self.pose_data["vx"].append(0.0)
-                self.pose_data["vy"].append(0.0)
-                self.pose_data["v"].append(0.0)
-                self.pose_data["dt"].append(0.0)
+
         # print(self.pose_data)
         return self.pose_data
 
@@ -237,6 +258,39 @@ class GraphPlotter:
         plt.cla()
         plt.close()
 
+    def make_velocity_graph(self) -> None:
+        fig , ax = plt.subplots()
+
+
+        # print(len(self.data_x))
+        # print(len(self.data_x[0]))
+        # print(len(self.data_y))
+        # print(len(self.data_y[0]))
+        for i in range(len(self.data_y)):
+            ax.plot(self.data_x, self.data_y[i], label=self.labels[i], linewidth=3)
+
+        ax.set_xlabel(self.label_x)#x軸のラベル
+        ax.set_ylabel(self.label_y)#y軸のラベル
+        ax.set_xlim(0.0, 15.0)#x軸の範囲
+        ax.set_ylim(-2.0, 2.4)#y軸の範囲
+        ax.set_xticks(np.arange(0.0, 15.0, 1.0))#x軸の目盛り
+        ax.set_yticks(np.arange(-1.8, 2.4, 0.4))#y軸の目盛り
+        # ax.minorticks_on()#補助目盛りを表示
+        ax.grid(which="major", color="black", linestyle="-")#目盛り線を表示
+        ax.grid(which="minor", color="gray", linestyle="--")#補助目盛り線を表示
+        ax.legend(loc="best")#凡例を表示
+        # ax.set_aspect("equal")#アスペクト比を1:1に
+        fig.set_figheight(6)#グラフの縦幅
+        fig.set_figwidth(15)#グラフの横幅
+        fig.tight_layout()#グラフの余白を調整
+
+        # fig.savefig(self.save_dir + self.graph_name, format="png", dpi=300)
+
+        plt.show()
+        plt.clf()
+        plt.cla()
+        plt.close()
+
 def create_label(_conditions: [], keys: []) -> []:
     labels = []
     for condition in _conditions:
@@ -272,6 +326,10 @@ if __name__ == "__main__":
     l_steer_data = []
     l_deg_steer_data = []
     steer_time = []
+    pose_time = []
+    v_data = []
+    vx_data = []
+    vy_data = []
 
     #add target trj
     target_data = BagReader(_bagfile_dir + "/" + _file_name_list[0], _file_name_list[0])
@@ -299,16 +357,28 @@ if __name__ == "__main__":
             r_deg_steer_data.append(data["steer"]["right_deg"])
             l_deg_steer_data.append(data["steer"]["left_deg"])
             steer_time.append(data["steer"]["time"])
+            pose_time.append(data["poses"]["time"])
+            v_data.append(data["poses"]["v"])
+            vx_data.append(data["poses"]["vx"])
+            vy_data.append(data["poses"]["vy"])
 
     labels = create_label(conditions, ["v", "L1", "L2", "t"])
     # print(labels)
 
     trj_graph = GraphPlotter(data_x, data_y, "x [m]", "y [m]", labels, "/home/amsl/master_thesis/thesis/pictures/trj_test_steer/","v1.2-steer.png")
     # trj_graph.make_trj_graph()
+
     steer_rad_graph = GraphPlotter(steer_time[0], [r_steer_data[0], l_steer_data[0]], "t [s]", "steer angle [rad]", ["right steer", "left steer"], "/home/amsl/master_thesis/thesis/pictures/trj_test_steer/","v1.2-steer.png")
     steer_rad_graph.make_steer_rad_graph()
     steer_rad_graph = GraphPlotter(steer_time[0], [r_deg_steer_data[0], l_deg_steer_data[0]], "t [s]", "steer angle [deg]", ["right steer", "left steer"], "/home/amsl/master_thesis/thesis/pictures/trj_test_steer/","v1.2-steer.png")
     steer_rad_graph.make_steer_deg_graph()
+
+    print(len(pose_time[0]))
+    print(len(v_data[0]))
+    print(len(vx_data[0]))
+    print(len(vy_data[0]))
+    velocity_graph = GraphPlotter(pose_time[0], [v_data[0], vx_data[0], vy_data[0]], "t [s]", "velocity [m/s]", ["v", "vx", "vy"], "/home/amsl/master_thesis/thesis/pictures/trj_test_steer/", "test_v.png")
+    velocity_graph.make_velocity_graph()
 
 
     print(labels)
